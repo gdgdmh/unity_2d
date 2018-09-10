967f80b6ea76c3ce0f1dff33746ab4bf0781d1c2
Kur0qb <gdgdkuro@gmail.com>
2018-09-10 23:31:35 +0900

    First Commit

---
+++ stg/Assets/Scripts/Common/Stg/ShareData.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/**
+ * シーン間共有データ
+ */
+public class ShareData : SingletonMonoBehaviour<ShareData>
+{
+
+    void Awake()
+    {
+        if (this != Instance)
+        {
+            // 既に存在しているなら破棄
+            Destroy(this);
+        }
+        else
+        {
+            // シーン遷移破棄させない
+            DontDestroyOnLoad(this.gameObject);
+        }
+        Initialize();
+    }
+
+    void OnDestroy()
+    {
+        // 破棄しておく
+        //game_parameter_ = null;
+    }
+
+    void Initialize()
+    {
+        //game_parameter_ = new GameParameter();
+        //game_parameter_.Initialize();
+
+    }
+
+    // ゲーム間でやり取りするデータ
+    //public GameParameter game_parameter_ { set; get; }
+}
---
+++ stg/Assets/Scripts/Stg/Game/PlayerController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    public float speed_ = 5.0f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        // 左右
+        //float x = Input.GetAxisRaw("Horizontal");
+        float x = Input.GetAxis("Horizontal");
+        // 上下
+        //float y = Input.GetAxisRaw("Vertical");
+        float y = Input.GetAxis("Vertical");
+
+        // 移動する向きを求める(正規化)
+        Vector2 direction = new Vector2(x, y).normalized;
+
+        // 移動するベクトル(向きと速さを)を適用
+        GetComponent<Rigidbody2D>().velocity = direction * speed_;
+	}
+}
